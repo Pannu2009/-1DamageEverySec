e4f9f982de333fee353730c4ba7259637c831773
Pannu2009 <Dakaitajay@gmail.com>
2026-09-13 12:35:30 -0700

    Refactor health values and add GetMaxHP function
    Updated health values formatting for consistency and added a function to retrieve maximum health points.

--- Shared/WallConfig.lua
+++ Shared/WallConfig.lua
@@ -3,29 +3,37 @@ local wallConfig = {}
 wallConfig.Groups = {
 	{
 		Name = "Wood_Zone",
-		Healths = {10,20,30,40,50},
+		Healths = {10, 20, 30, 40, 50},
 		WinReward = 2,
 	},
 	{
 		Name = "Stone_Zone",
-		Healths = {70,90,110,130,150},
+		Healths = {70, 90, 110, 130, 150},
 		WinReward = 5,
 	},
 	{
 		Name = "Iron_Zone",
-		Healths = {180,210,240,270,300},
+		Healths = {180, 210, 240, 270, 300},
 		WinReward = 10,
 	},
 	{
 		Name = "Metal_Zone",
-		Healths = {340,380,420,460,500},
+		Healths = {340, 380, 420, 460, 500},
 		WinReward = 25,
 	},
 	{
 		Name = "Zinc_Zone",
-		Healths = {550,600,650,700,750},
+		Healths = {550, 600, 650, 700, 750},
 		WinReward = 40,
 	}
 }
+
+function wallConfig.GetMaxHP(groupIndex, wallIndex)
+	local group = wallConfig.Groups[groupIndex]
+	if group and group.Healths then
+		return group.Healths[wallIndex] or 0
+	end
+	return 0
+end
 
 return wallConfig
