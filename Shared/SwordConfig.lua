a60e74068a87744e4183668a35de6251718af794
Pannu2009 <Dakaitajay@gmail.com>
2026-09-17 11:15:50 -0700

    Update sword multipliers and player speed in config

--- Shared/SwordConfig.lua
+++ Shared/SwordConfig.lua
@@ -1,16 +1,13 @@
 local SwordConfig = {}
 
--- All sword multipliers live here.
--- The default ServerStorage tool is named "sword".
--- When MainServer gives it to a player, it is tagged as the Rusty Sword.
 
 SwordConfig.DefaultSword = "Rusty Sword"
 SwordConfig.DefaultModelName = "sword"
 SwordConfig.AttackCooldown = 0.4
 
 SwordConfig.Swords = {
-	{Name = "Rusty Sword", Cost = 0,   Multi = 0.5},
-	{Name = "Iron Blade",  Cost = 500, Multi = 1.5},
+	{Name = "Rusty Sword", Cost = 0,   Multi = 1, plrSpeed = 16,},
+	{Name = "Iron Blade",  Cost = 500, Multi = 1.5, plrSpeed = 16,},
 	{Name = "Void Blade",  Cost = 0,   Multi = 7.0, BossDrop = true},
 }
 
